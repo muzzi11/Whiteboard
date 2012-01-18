8972910c2681ae9bcc103e948df2db2a1cd088ac
Michiel Folkers <mcf.work@gmail.com>
2012-01-18 04:20:30 +0100

    Added a basic class wrapper for Ajax calls.

--- utilities/js/tools.js
+++ utilities/js/tools.js
@@ -60,27 +60,96 @@ Element.prototype.insert = function(h) {
  	this.parentNode.replaceChild(e, this);
  }
  
+ /**
+ * Replace the element's style with another style.
+ * @param o Object of containing the style with which to replace the current style.
+ */
  Element.prototype.css = function(o) {
 	this.class.style = o;
 	return this.class;
 }
 
+/**
+* Add a class to the class attribute of an element.
+* @param s String of the class name to add.
+*
+*/
 Element.prototype.addClass = function(s) {
 	classList = this.getClass() != '' && this.getClass() != null ? this.getClass() +' '+ s : s;
 	this.setClass(classList);
 }
 
+/**
+* Set the class attribute of an element.
+* @param s String of the class name.
+*/
 Element.prototype.setClass = function(s) {
 	this.setAttribute('class', s);
 }
 
+/**
+* Return the class attribute of an element.
+*/
 Element.prototype.getClass = function() {
 	return this.getAttribute('class');
 }
 
+/**
+* Remove a class from an element.
+* @param s String to represent the regular expression of the class to remove.
+*
+*/
 Element.prototype.removeClass = function(s) {
 	classList = this.getClass();
 	var reg = new RegExp(s, 'g');
 	classList = classList.replace(reg, '');
 	this.setClass(classList);
+}
+
+/**
+* Hide element by setting the display to 'none'.
+*/
+Element.prototype.hide = function() {
+	this.style.display = "none";
+}
+
+/**
+* Show element by removing 'none'.
+*/
+Element.prototype.show = function() {
+	if(this.style.display == "none") {
+		this.style.display = "";
+	}
+}
+
+Ajax = function() {
+	this.httpRequestObject = null;
+	
+	Ajax.prototype.init = function () {
+		this.httpRequestObject = this.getHttpRequestObject();
+	}
+	
+	Ajax.prototype.getHttpRequestObject = function() {
+		var httpRequest = null;
+		if (window.XMLHttpRequest) { // Mozilla, Safari, ...  
+			httpRequest = new XMLHttpRequest();
+		} else if (window.ActiveXObject) { // IE
+			try {
+				httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+			}
+			catch (e) {
+				try {
+					httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+				}
+				catch (e) {}
+			}
+		}
+		if (!httpRequest) {
+			alert('Giving up :( Cannot create an XMLHTTP instance');
+			return false;
+		}
+		return httpRequest;
+	}
+	
+	this.init();
 }

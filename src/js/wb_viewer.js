0cb53e905981d2f0a23a8ae673064fa3d5dbdfb8
Michiel Folkers <mcf.work@gmail.com>
2012-01-26 17:46:16 +0100

    Fixed and expanded comment function.
    The comments should now indent and flow properly under eachother with the replies to another comment coming underneath the parent. Clicking on the comment number or NEW POST should bring up a fixed position input panel to post the comment.

--- src/js/wb_viewer.js
+++ src/js/wb_viewer.js
@@ -40,6 +40,8 @@ Viewer = function() {
 		document.loadContent = function(page_id) {
 			$(document.viewer.article).page_id = page_id;
 			$('#textarea').parent = null;
+			$('#reply').hide();
+			$('#comments').insert('');
 			document.userID = '1';
 			document.api.loadContent(page_id, document.genContent);
 			document.api.loadComments(page_id, document.userID, document.genComments);
@@ -120,12 +122,30 @@ Viewer = function() {
                 
 		document.genComments = function(comments) {
 			//alert(comments);
-			$('#comments').insert('');
+			$('#comments').insert("<a href='#' onclick='document.reply(null)'>POST NEW</a>");
 			if(comments != ''){
 			comments = eval('('+comments+')');
+			var comment = "<section style='margin-left:{indent}px;'><b>{user}</b>	{date}	<a href='#' onclick='document.reply({id})'>No.{nr}</a><hr />{text}</section>";
+			var count = 0;
 			for (c in comments) {
-				comments[c].nr = c;
-				$('#comments').insert({bottom:"<h3>POST:	{nr}	REPLY TO:	{parent}	\nUSER:	{user}	DATE:	{date}</h3><section>{text}<input type='checkbox' value='{id}' onclick='document.reply(this.value)'/></section>".interpolate(comments[c])});
+				count++;
+				comments[c].nr = count;
+				if (comments[c].parent == null) {
+					$('#comments').insert({bottom:comment.interpolate(comments[c])});
+					var reply = function(coments, id, level) {
+						var count = 0;
+						for (r in comments) {
+							count++;
+							if(id == comments[r].parent) {
+								comments[r].indent = level * 50;
+								comments[r].nr = count;
+								$('#comments').insert({bottom:comment.interpolate(comments[r])});
+								reply(comments, comments[r].id, level+1);
+							}
+						}
+					}
+					reply(comments, comments[c].id, 1);
+				}
 			}//alert('');
 			$('#textarea').value = '';
 		}
@@ -134,13 +154,19 @@ Viewer = function() {
 			document.postComment = function() {
 				//alert($('#textarea').parent);
 				//alert("PAGE:	"+$('article').page_id+'\nUSER:	'+document.userID+'\nPOST:	'+$('#textarea').value+'\n');
-			document.api.postComment($('article').page_id, document.userID, $('textarea').value, document.genComments, $('textarea').parent);
-			$('#textarea').parent = null;
+			document.api.postComment($(document.viewer.article).page_id, document.userID, $('textarea').value, document.genComments, $('textarea').parent);
+			document.postClose();
 
 		//document.sitemap = elems;
 	}
+	document.postClose = function() {
+		$('#textarea').parent = null;
+		$('#textarea').value = '';
+		$('#reply').hide();
+	}
 	document.reply = function(parent) {
 		$('#textarea').parent = parent;
+		$('#reply').show();
 	}
 
 	Viewer.prototype.reSubMenu = function(nr, content) {
